svn2github/desmume

commit e423956f09b6cba2828a7021792b8a1982d1480c

Cocoa Port: - Read the GPU_screen buffer directly and avoid making multiple copies of it. Improves performance when many display windows are used.
git-svn-id: svn://svn.code.sf.net/p/desmume/code/trunk@5083 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/cocoa/OGLDisplayOutput.cpp
@@ -1470,11 +1470,11 @@ void OGLVideoOutput::SetViewportSizeOGL(GLsizei w, GLsizei h)
 	}
 }
 
-void OGLVideoOutput::ProcessOGL(const uint16_t *videoData, GLsizei w, GLsizei h)
+void OGLVideoOutput::ProcessOGL()
 {
 	for (size_t i = 0; i < _layerList->size(); i++)
 	{
-		(*_layerList)[i]->ProcessOGL(videoData, w, h);
+		(*_layerList)[i]->ProcessOGL();
 	}
 }
 
@@ -1736,6 +1736,9 @@ OGLDisplayLayer::OGLDisplayLayer(OGLVideoOutput *oglVO)
 	_displayOrder = DS_DISPLAY_ORDER_MAIN_FIRST;
 	_displayOrientation = DS_DISPLAY_ORIENTATION_VERTICAL;
 	
+	_vtxElementCount = (_displayMode == DS_DISPLAY_TYPE_DUAL) ? 12 : 6;
+	_vtxElementPointer = !(_displayMode == DS_DISPLAY_TYPE_TOUCH) ? 0 : (GLubyte *)(_vtxElementCount * sizeof(GLubyte));
+	
 	_normalWidth = GPU_DISPLAY_WIDTH;
 	_normalHeight = GPU_DISPLAY_HEIGHT*2.0 + (DS_DISPLAY_GAP*_gapScalar);
 	_rotation = 0.0f;
@@ -1756,9 +1759,10 @@ OGLDisplayLayer::OGLDisplayLayer(OGLVideoOutput *oglVO)
 	
 	// Set up textures
 	glGenTextures(1, &_texCPUFilterDstID);
-	glGenTextures(1, &_texInputVideoDataID);
-	_texOutputVideoDataID = _texInputVideoDataID;
-	_texPrevOutputVideoDataID = _texInputVideoDataID;
+	glGenTextures(1, &_texVideoInputDataID);
+	_texVideoSourceID = _texVideoInputDataID;
+	_texVideoPixelScalerID = _texVideoInputDataID;
+	_texVideoOutputID = _texVideoInputDataID;
 	
 	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, _texCPUFilterDstID);
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -1768,7 +1772,7 @@ OGLDisplayLayer::OGLDisplayLayer(OGLVideoOutput *oglVO)
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_STORAGE_HINT_APPLE, GL_STORAGE_CACHED_APPLE);
 	glTextureRangeAPPLE(GL_TEXTURE_RECTANGLE_ARB, _vfDual->GetDstWidth() * _vfDual->GetDstHeight() * sizeof(uint32_t), _vfMasterDstBuffer);
 	
-	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, _texInputVideoDataID);
+	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, _texVideoInputDataID);
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -1865,7 +1869,7 @@ OGLDisplayLayer::~OGLDisplayLayer()
 	glDeleteBuffersARB(1, &this->_vboTexCoordID);
 	glDeleteBuffersARB(1, &this->_vboElementID);
 	glDeleteTextures(1, &this->_texCPUFilterDstID);
-	glDeleteTextures(1, &this->_texInputVideoDataID);
+	glDeleteTextures(1, &this->_texVideoInputDataID);
 	
 	if (_canUseShaderOutput)
 	{
@@ -1908,15 +1912,21 @@ void OGLDisplayLayer::SetMode(int dispMode)
 		case DS_DISPLAY_TYPE_MAIN:
 			this->_vfSingle->SetDstBufferPtr(_vfMasterDstBuffer);
 			this->_vf = this->_vfSingle;
+			this->_vtxElementCount = 6;
+			this->_vtxElementPointer = 0;
 			break;
 			
 		case DS_DISPLAY_TYPE_TOUCH:
 			this->_vfSingle->SetDstBufferPtr(_vfMasterDstBuffer + (this->_vfSingle->GetDstWidth() * this->_vfSingle->GetDstHeight()) );
 			this->_vf = this->_vfSingle;
+			this->_vtxElementCount = 6;
+			this->_vtxElementPointer = (GLubyte *)(this->_vtxElementCount * sizeof(GLubyte));
 			break;
 			
 		case DS_DISPLAY_TYPE_DUAL:
 			this->_vf = this->_vfDual;
+			this->_vtxElementCount = 12;
+			this->_vtxElementPointer = 0;
 			break;
 			
 		default:
@@ -2279,8 +2289,8 @@ void OGLDisplayLayer::SetCPUFilterOGL(const VideoFilterTypeID videoFilterTypeID)
 	const VideoFilterAttributes newFilterAttr = VideoFilter::GetAttributesByID(videoFilterTypeID);
 	const size_t oldDstBufferWidth = this->_vfDual->GetDstWidth();
 	const size_t oldDstBufferHeight = this->_vfDual->GetDstHeight();
-	const GLsizei newDstBufferWidth = GPU_DISPLAY_WIDTH * newFilterAttr.scaleMultiply / newFilterAttr.scaleDivide;
-	const GLsizei newDstBufferHeight = GPU_DISPLAY_HEIGHT * 2 * newFilterAttr.scaleMultiply / newFilterAttr.scaleDivide;
+	const GLsizei newDstBufferWidth = this->_vfDual->GetSrcWidth() * newFilterAttr.scaleMultiply / newFilterAttr.scaleDivide;
+	const GLsizei newDstBufferHeight = this->_vfDual->GetSrcHeight() * newFilterAttr.scaleMultiply / newFilterAttr.scaleDivide;
 	
 	if (oldDstBufferWidth != newDstBufferWidth || oldDstBufferHeight != newDstBufferHeight)
 	{
@@ -2327,73 +2337,80 @@ void OGLDisplayLayer::SetCPUFilterOGL(const VideoFilterTypeID videoFilterTypeID)
 	this->_vfDual->ChangeFilterByID(videoFilterTypeID);
 }
 
-void OGLDisplayLayer::ProcessOGL(const uint16_t *videoData, GLsizei w, GLsizei h)
+void OGLDisplayLayer::LoadFrameOGL(const uint16_t *frameData, GLsizei w, GLsizei h)
+{
+	const GLint lineOffset = (this->_displayMode == DS_DISPLAY_TYPE_TOUCH) ? h : 0;
+	const bool isUsingCPUPixelScaler = this->_pixelScaler != VideoFilterTypeID_None && !(this->_useShaderBasedPixelScaler && this->_filtersPreferGPU);
+	
+	if (!isUsingCPUPixelScaler)
+	{
+		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoInputDataID);
+		glTexSubImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, 0, lineOffset, w, h, GL_RGBA, GL_UNSIGNED_SHORT_1_5_5_5_REV, frameData);
+		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, 0);
+	}
+	else
+	{
+		RGB555ToBGRA8888Buffer((const uint16_t *)frameData, this->_vf->GetSrcBufferPtr(), w * h);
+	}
+}
+
+void OGLDisplayLayer::ProcessOGL()
 {
 	VideoFilter *currentFilter = this->_vf;
-	GLint lineOffset = (this->_displayMode == DS_DISPLAY_TYPE_TOUCH) ? h : 0;
+	const bool isUsingCPUPixelScaler = this->_pixelScaler != VideoFilterTypeID_None && !(this->_useShaderBasedPixelScaler && this->_filtersPreferGPU);
 	
-	// Determine whether we should take CPU-based path or a GPU-based path
-	if	(this->_pixelScaler != VideoFilterTypeID_None && !(this->_useShaderBasedPixelScaler && this->_filtersPreferGPU) )
+	// Source
+	if (this->_useDeposterize)
 	{
-		if (!this->_useDeposterize) // Pure CPU-based path
+		this->_texVideoSourceID = this->_filterDeposterize->RunFilterOGL(this->_texVideoInputDataID, this->_viewportWidth, this->_viewportHeight);
+		
+		if (isUsingCPUPixelScaler) // Hybrid CPU/GPU-based path (may cause a performance hit on pixel download)
 		{
-			RGB555ToBGRA8888Buffer((const uint16_t *)videoData, (uint32_t *)currentFilter->GetSrcBufferPtr(), w * h);
-		}
-		else // Hybrid CPU/GPU-based path (may cause a performance hit on pixel download)
-		{
-			glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texInputVideoDataID);
-			glTexSubImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, 0, lineOffset, w, h, GL_RGBA, GL_UNSIGNED_SHORT_1_5_5_5_REV, videoData);
-			
-			this->_filterDeposterize->RunFilterOGL(this->_texInputVideoDataID, this->_viewportWidth, this->_viewportHeight);
-			
+			const GLint lineOffset = (this->_displayMode == DS_DISPLAY_TYPE_TOUCH) ? GPU_DISPLAY_HEIGHT : 0;
 			const GLsizei readLineCount = (this->_displayMode == DS_DISPLAY_TYPE_DUAL) ? GPU_DISPLAY_HEIGHT * 2 : GPU_DISPLAY_HEIGHT;
 			this->_filterDeposterize->DownloadDstBufferOGL(currentFilter->GetSrcBufferPtr(), lineOffset, readLineCount);
 		}
-		
-		uint32_t *texData = currentFilter->RunFilter();
-		w = currentFilter->GetDstWidth();
-		h = currentFilter->GetDstHeight();
-		lineOffset = (this->_displayMode == DS_DISPLAY_TYPE_TOUCH) ? h : 0;
-		
-		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texCPUFilterDstID);
-		glTexSubImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, 0, lineOffset, w, h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, texData);
-		
-		this->_texOutputVideoDataID = this->_texCPUFilterDstID;
-		this->UpdateTexCoords(w, (this->_displayMode == DS_DISPLAY_TYPE_DUAL) ? h : h*2);
 	}
-	else // Pure GPU-based path
+	else
 	{
-		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texInputVideoDataID);
-		glTexSubImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, 0, lineOffset, w, h, GL_RGBA, GL_UNSIGNED_SHORT_1_5_5_5_REV, videoData);
-		
-		if (this->_useDeposterize)
-		{
-			this->_texOutputVideoDataID = this->_filterDeposterize->RunFilterOGL(this->_texInputVideoDataID, this->_viewportWidth, this->_viewportHeight);
-		}
-		else
-		{
-			this->_texOutputVideoDataID = this->_texInputVideoDataID;
-		}
-		
-		if (this->_displayMode != DS_DISPLAY_TYPE_DUAL)
-		{
-			h *= 2;
-		}
-		
+		this->_texVideoSourceID = this->_texVideoInputDataID;
+	}
+	
+	// Pixel scaler
+	if (!isUsingCPUPixelScaler)
+	{
 		if (this->_useShaderBasedPixelScaler)
 		{
-			glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texOutputVideoDataID);
+			glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoSourceID);
 			glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 			glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 			
-			this->_texOutputVideoDataID = this->_shaderFilter->RunFilterOGL(this->_texOutputVideoDataID, this->_viewportWidth, this->_viewportHeight);
-			w = this->_shaderFilter->GetDstWidth();
-			h = this->_shaderFilter->GetDstHeight();
+			this->_texVideoPixelScalerID = this->_shaderFilter->RunFilterOGL(this->_texVideoSourceID, this->_viewportWidth, this->_viewportHeight);
+			
+			this->UpdateTexCoords(this->_shaderFilter->GetDstWidth(), this->_shaderFilter->GetDstHeight());
 		}
+		else
+		{
+			this->_texVideoPixelScalerID = this->_texVideoSourceID;
+			this->UpdateTexCoords(GPU_DISPLAY_WIDTH, GPU_DISPLAY_HEIGHT*2);
+		}
+	}
+	else
+	{
+		uint32_t *texData = currentFilter->RunFilter();
 		
-		this->UpdateTexCoords(w, h);
+		const GLfloat w = currentFilter->GetDstWidth();
+		const GLfloat h = currentFilter->GetDstHeight();
+		const GLsizei lineOffset = (this->_displayMode == DS_DISPLAY_TYPE_TOUCH) ? h : 0;
+		this->_texVideoPixelScalerID = this->_texCPUFilterDstID;
+		
+		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoPixelScalerID);
+		glTexSubImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, 0, lineOffset, w, h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, texData);
+		this->UpdateTexCoords(w, (this->_displayMode == DS_DISPLAY_TYPE_DUAL) ? h : h*2);
 	}
 	
+	// Output
+	this->_texVideoOutputID = this->_texVideoPixelScalerID;
 	this->UploadTexCoordsOGL();
 	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, 0);
 }
@@ -2403,7 +2420,7 @@ void OGLDisplayLayer::RenderOGL()
 	glUseProgram(this->_finalOutputProgram->GetProgramID());
 	this->UploadTransformationOGL();
 	
-	if (_needUploadVertices)
+	if (this->_needUploadVertices)
 	{
 		this->UploadVerticesOGL();
 	}
@@ -2411,14 +2428,11 @@ void OGLDisplayLayer::RenderOGL()
 	// Enable vertex attributes
 	glBindVertexArrayAPPLE(this->_vaoMainStatesID);
 	
-	const GLsizei vtxElementCount = (this->_displayMode == DS_DISPLAY_TYPE_DUAL) ? 12 : 6;
-	const GLubyte *elementPointer = !(this->_displayMode == DS_DISPLAY_TYPE_TOUCH) ? 0 : (GLubyte *)(vtxElementCount * sizeof(GLubyte));
-	
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texOutputVideoDataID);
+	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoOutputID);
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, this->_displayTexFilter);
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, this->_displayTexFilter);
-	glDrawElements(GL_TRIANGLES, vtxElementCount, GL_UNSIGNED_BYTE, elementPointer);
+	glDrawElements(GL_TRIANGLES, this->_vtxElementCount, GL_UNSIGNED_BYTE, this->_vtxElementPointer);
 	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, 0);
 	
 	// Disable vertex attributes

desmume/src/cocoa/OGLDisplayOutput.h
@@ -185,7 +185,7 @@ public:
 		this->_viewportHeight = h;
 	};
 	
-	virtual void ProcessOGL(const uint16_t *videoData, GLsizei w, GLsizei h) {};
+	virtual void ProcessOGL() {};
 	virtual void RenderOGL() {};
 };
 
@@ -219,6 +219,9 @@ protected:
 	GLfloat _gapScalar;
 	GLfloat _rotation;
 	
+	GLsizei _vtxElementCount;
+	GLubyte *_vtxElementPointer;
+	
 	GLint _displayTexFilter;
 	GLuint _texCPUFilterDstID;
 	
@@ -226,9 +229,10 @@ protected:
 	GLfloat texCoordBuffer[2 * 8];
 	size_t _vtxBufferOffset;
 	
-	GLuint _texInputVideoDataID;
-	GLuint _texOutputVideoDataID;
-	GLuint _texPrevOutputVideoDataID;
+	GLuint _texVideoInputDataID;
+	GLuint _texVideoSourceID;
+	GLuint _texVideoPixelScalerID;
+	GLuint _texVideoOutputID;
 	GLuint _vaoMainStatesID;
 	GLuint _vboVertexID;
 	GLuint _vboTexCoordID;
@@ -275,7 +279,8 @@ public:
 	int GetPixelScaler();
 	virtual void SetPixelScalerOGL(const int filterID);
 	virtual void SetCPUFilterOGL(const VideoFilterTypeID videoFilterTypeID);
-	virtual void ProcessOGL(const uint16_t *videoData, GLsizei w, GLsizei h);
+	virtual void LoadFrameOGL(const uint16_t *frameData, GLsizei w, GLsizei h);
+	virtual void ProcessOGL();
 	virtual void RenderOGL();
 };
 
@@ -297,7 +302,7 @@ public:
 	virtual GLsizei GetViewportHeight();
 	OGLDisplayLayer* GetDisplayLayer();
 		
-	virtual void ProcessOGL(const uint16_t *videoData, GLsizei w, GLsizei h);
+	virtual void ProcessOGL();
 	virtual void RenderOGL();
 	virtual void SetViewportSizeOGL(GLsizei w, GLsizei h);
 };

desmume/src/cocoa/cocoa_GPU.h
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2013-2014 DeSmuME team
+	Copyright (C) 2013-2015 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -70,6 +70,7 @@ extern "C"
 {
 #endif
 
+void GPU_FillScreenWithBGRA5551(const uint16_t colorValue);
 void SetGPULayerState(const int gpuType, const unsigned int i, const bool state);
 bool GetGPULayerState(const int gpuType, const unsigned int i);
 void SetGPUDisplayState(const int gpuType, const bool state);

desmume/src/cocoa/cocoa_GPU.mm
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2013-2014 DeSmuME team
+	Copyright (C) 2013-2015 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -99,6 +99,8 @@ GPU3DInterface *core3DList[] = {
 	SetOpenGLRendererFunctions(&OSXOpenGLRendererInit,
 							   &OSXOpenGLRendererBegin,
 							   &OSXOpenGLRendererEnd);
+	
+	GPU_FillScreenWithBGRA5551(0x8000);
 	
 	return self;
 }
@@ -819,6 +821,26 @@ bool GetGPUDisplayState(const int gpuType)
 	}
 	
 	return theState;
+}
+
+void GPU_FillScreenWithBGRA5551(const uint16_t colorValue)
+{
+	const size_t pixCount = sizeof(GPU_screen) / sizeof(uint16_t);
+	
+#ifdef __APPLE__
+	if (pixCount % 16 == 0)
+	{
+		const uint16_t colorValuePattern[] = {colorValue, colorValue, colorValue, colorValue, colorValue, colorValue, colorValue, colorValue};
+		memset_pattern16(GPU_screen, colorValuePattern, sizeof(GPU_screen));
+	}
+	else
+#endif
+	{
+		for (size_t i = 0; i < pixCount; i++)
+		{
+			((uint16_t *)GPU_screen)[i] = colorValue;
+		}
+	}
 }
 
 CGLContextObj OSXOpenGLRendererContext = NULL;

desmume/src/cocoa/cocoa_core.mm
@@ -890,6 +890,7 @@ volatile bool execute = true;
 	
 	pthread_mutex_lock(&threadParam.mutexThreadExecute);
 	NDS_Reset();
+	GPU_FillScreenWithBGRA5551(0xFFFF);
 	pthread_mutex_unlock(&threadParam.mutexThreadExecute);
 	
 	[self restoreCoreState];

desmume/src/cocoa/cocoa_globals.h
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2011 Roger Manuel
-	Copyright (C) 2012-2014 DeSmuME Team
+	Copyright (C) 2012-2015 DeSmuME Team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -438,8 +438,6 @@ enum
 	MESSAGE_SET_RENDER3D_THREADS,
 	MESSAGE_SET_RENDER3D_LINE_HACK,
 	MESSAGE_SET_RENDER3D_MULTISAMPLE,
-	MESSAGE_SET_VIEW_TO_BLACK,
-	MESSAGE_SET_VIEW_TO_WHITE,
 	
 	MESSAGE_SET_AUDIO_PROCESS_METHOD,
 	MESSAGE_SET_SPU_ADVANCED_LOGIC,

desmume/src/cocoa/cocoa_output.h
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2011 Roger Manuel
-	Copyright (C) 2011-2014 DeSmuME team
+	Copyright (C) 2011-2015 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -36,18 +36,17 @@ typedef struct
 
 typedef struct
 {
-	int32_t videoSourceID;
-	int32_t displayModeID;
-	uint16_t width;			// Measured in pixels
-	uint16_t height;		// Measured in pixels
-} DisplaySrcPixelAttributes;
+	uint16_t	*buffer;			// Pointer to frame buffer
+	size_t		bufferSize;			// Size (in bytes) of frame buffer
+	int32_t		displayModeID;		// The selected display to read from
+	uint16_t	width;				// Measured in pixels
+	uint16_t	height;				// Measured in pixels
+} GPUFrame;
 
 @interface CocoaDSOutput : CocoaDSThread
 {
 	BOOL isStateChanged;
 	NSUInteger frameCount;
-	NSData *frameData;
-	NSData *frameAttributesData;
 	NSMutableDictionary *property;
 	
 	pthread_mutex_t *mutexConsume;
@@ -56,14 +55,12 @@ typedef struct
 
 @property (assign) BOOL isStateChanged;
 @property (assign) NSUInteger frameCount;
-@property (retain) NSData *frameData;
-@property (retain) NSData *frameAttributesData;
 @property (readonly) NSMutableDictionary *property;
 @property (assign) pthread_rwlock_t *rwlockProducer;
 @property (readonly) pthread_mutex_t *mutexConsume;
 
 - (void) doCoreEmuFrame;
-- (void) handleEmuFrameProcessed:(NSData *)mainData attributes:(NSData *)attributesData;
+- (void) handleEmuFrameProcessed;
 
 @end
 
@@ -127,7 +124,8 @@ typedef struct
 
 @required
 - (void) doInitVideoOutput:(NSDictionary *)properties;
-- (void) doProcessVideoFrame:(const void *)videoFrameData displayMode:(const NSInteger)frameDisplayMode width:(const NSInteger)frameWidth height:(const NSInteger)frameHeight;
+- (void) doLoadVideoFrame:(const void *)videoFrameData displayMode:(const NSInteger)frameDisplayMode width:(const NSInteger)frameWidth height:(const NSInteger)frameHeight;
+- (void) doProcessVideoFrame;
 
 @optional
 - (void) doResizeView:(NSRect)rect;
@@ -145,6 +143,7 @@ typedef struct
 	id <CocoaDSDisplayDelegate> delegate;
 	NSInteger displayMode;
 	NSSize frameSize;
+	GPUFrame _gpuFrame;
 	
 	OSSpinLock spinlockDisplayType;
 }
@@ -154,13 +153,8 @@ typedef struct
 @property (readonly) NSSize frameSize;
 
 - (void) handleChangeDisplayMode:(NSData *)displayModeData;
-- (void) handleSetViewToBlack;
-- (void) handleSetViewToWhite;
 - (void) handleRequestScreenshot:(NSData *)fileURLStringData fileTypeData:(NSData *)fileTypeData;
 - (void) handleCopyToPasteboard;
-
-- (NSData *) videoFrameUsingRGBA5551:(uint16_t)colorValue pixelCount:(size_t)pixCount;
-- (void) sendVideoFrameOfRGBA5551:(uint16_t)colorValue;
 
 - (NSImage *) image;
 - (NSBitmapImageRep *) bitmapImageRep;

commit 21274cee52c03247b9e507617c6528316d68fb2f

Cocoa Port: - Fix bug where using Deposterize with a CPU-based pixel scaler would make the video fail to update. (Regression from r5083.)
git-svn-id: svn://svn.code.sf.net/p/desmume/code/trunk@5085 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/cocoa/OGLDisplayOutput.cpp
@@ -2342,7 +2342,7 @@ void OGLDisplayLayer::LoadFrameOGL(const uint16_t *frameData, GLsizei w, GLsizei
 	const GLint lineOffset = (this->_displayMode == DS_DISPLAY_TYPE_TOUCH) ? h : 0;
 	const bool isUsingCPUPixelScaler = this->_pixelScaler != VideoFilterTypeID_None && !(this->_useShaderBasedPixelScaler && this->_filtersPreferGPU);
 	
-	if (!isUsingCPUPixelScaler)
+	if (!isUsingCPUPixelScaler || this->_useDeposterize)
 	{
 		glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoInputDataID);
 		glTexSubImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, 0, lineOffset, w, h, GL_RGBA, GL_UNSIGNED_SHORT_1_5_5_5_REV, frameData);